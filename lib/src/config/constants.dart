385d76422099aa813987d16620138e3f3e70c02a
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-11-01 17:43:32 -0600

    added about page

--- lib/src/config/constants.dart
+++ lib/src/config/constants.dart
@@ -1,3 +1,6 @@
 class Constants {
   static String tokenStorageKey = 'tokenStorageKey';
+  static String supportEmail = 'support@example.com';
+  static String privacyPolicyUrl = 'https://google.com';
+  static String termsOfServiceUrl = 'https://google.com';
 }
--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 ThemeData flutterAppTheme = ThemeData(
-  accentColor: Color.fromRGBO(200, 200, 200, 1),
-  primaryColor: Color.fromRGBO(90, 90, 90, 1),
-  appBarTheme: AppBarTheme(iconTheme: IconThemeData(color: Colors.white)),
-);
+    accentColor: Color.fromRGBO(200, 200, 200, 1),
+    primaryColor: Color.fromRGBO(90, 90, 90, 1),
+    appBarTheme: AppBarTheme(iconTheme: IconThemeData(color: Colors.white)),
+    primaryTextTheme: TextTheme(
+        body1: flutterAppTheme.primaryTextTheme.body1
+            .copyWith(color: Colors.white),
+        body2: flutterAppTheme.primaryTextTheme.body1.copyWith(
+            color: Colors.lightBlue, decoration: TextDecoration.underline),
+        headline: flutterAppTheme.primaryTextTheme.headline
+            .copyWith(color: Colors.white)));
