8fb3367a7ba17cd806cdafdae495815ba18fc045
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-01-04 00:03:17 -0700

    resolved conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_base_app/src/FlutterApp.dart';
 
-void main() => runApp(FlutterApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(FlutterApp());
+}
