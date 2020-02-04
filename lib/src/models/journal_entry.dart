43f733968c2b06871d11dbe3573ff4596ee9150f
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-02-03 22:05:51 -0700

    file renaming. converted dates to iso8601 timestamps instead of milliseconds since epoch

---
+++ lib/src/models/journal_entry.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+
+import 'package:grateful/src/models/photograph.dart';
+import 'package:uuid/uuid.dart';
+
+class JournalEntry {
+  String id;
+  String body;
+  String description;
+  DateTime date;
+  List<NetworkPhoto> photographs;
+  JournalEntry({
+    String id,
+    this.body,
+    this.description,
+    DateTime date,
+    List<NetworkPhoto> photographs,
+  })  : this.id = id ?? Uuid().v4(),
+        this.date = date ?? DateTime.now(),
+        this.photographs = photographs ?? <NetworkPhoto>[];
+
+  JournalEntry copyWith({
+    String id,
+    String body,
+    String description,
+    DateTime date,
+    List<NetworkPhoto> photographs,
+  }) {
+    return JournalEntry(
+      id: id ?? this.id,
+      body: body ?? this.body,
+      description: description ?? this.description,
+      date: date ?? this.date,
+      photographs: photographs ?? this.photographs,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'body': body,
+      'description': description,
+      'date': date.toUtc().toIso8601String(),
+      'photographs': List<dynamic>.from(photographs.map((x) => x.toMap())),
+    };
+  }
+
+  static DateTime _parseDate(dynamic datestamp) {
+    if (datestamp is int) {
+      return DateTime.fromMillisecondsSinceEpoch(datestamp);
+    } else if (datestamp is String) {
+      return DateTime.tryParse(datestamp).toLocal();
+    }
+    return null;
+  }
+
+  static JournalEntry fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return JournalEntry(
+      id: map['id'],
+      body: map['body'],
+      description: map['description'],
+      date: _parseDate(map['date']),
+      photographs: List<NetworkPhoto>.from(
+          map['photographs']?.map((x) => NetworkPhoto.fromMap(x))),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  static JournalEntry fromJson(String source) => fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'JournalEntry id: $id, body: $body, description: $description, date: $date, photographs: $photographs';
+  }
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is JournalEntry &&
+        o.id == id &&
+        o.body == body &&
+        o.description == description &&
+        o.date == date &&
+        o.photographs == photographs;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        body.hashCode ^
+        description.hashCode ^
+        date.hashCode ^
+        photographs.hashCode;
+  }
+}
